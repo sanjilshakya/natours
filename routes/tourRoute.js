0525c72bfe3149c4046d88a39b70dffd02d5c855
Sanjil Shakya <sanjil@noveltytechnology.com>
2024-05-31 15:51:28 -0400

    aggregation pipeline added for tour-stats and monthly-plan routes

--- routes/tourRoute.js
+++ routes/tourRoute.js
@@ -16,9 +16,13 @@ const {
   getTour,
   updateTour,
   deleteTour,
+  getTourStats,
+  getMonthlyPlan,
 } = require("../controllers/tourController-refactor");
 
 router.route("/top-5-tours").get(aliasTopTours, getTours);
+router.route("/tour-stats").get(getTourStats);
+router.route("/monthly-plan/:year").get(getMonthlyPlan);
 router.route("/").get(getTours).post(createTour);
 router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
 
