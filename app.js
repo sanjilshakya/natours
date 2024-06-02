54725d0f0579bc20c5a457af7975f7f5ca3755de
Sanjil Shakya <sanjil@noveltytechnology.com>
2024-06-02 05:38:36 -0400

    handling unhandled routes

--- app.js
+++ app.js
@@ -7,4 +7,11 @@ app.use(express.json());
 
 app.use("/api/v1/tours", tourRouter);
 
+app.all("*", (req, res, next) => {
+  res.status(400).json({
+    status: "fail",
+    message: `Cannot find ${req.originalUrl} in this server`,
+  });
+});
+
 module.exports = app;
