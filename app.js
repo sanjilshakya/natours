de4a11057e01b1586d85ade934ae6be9236704a7
Sanjil Shakya <sanjil@noveltytechnology.com>
2024-06-02 06:16:28 -0400

    error handler refactored

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const tourRouter = require("./routes/tourRoute");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 
 const app = express();
 
@@ -9,27 +11,10 @@ app.use("/api/v1/tours", tourRouter);
 
 //Handling Unhandled Routes
 app.all("*", (req, res, next) => {
-  //   res.status(404).json({
-  //     status: "fail",
-  //     message: `Cannot find ${req.originalUrl} in this server`,
-  //   });
-
-  const error = new Error(`Cannot find ${req.originalUrl} in this server`);
-  error.status = "fail";
-  error.statusCode = 404;
-
-  next(error);
+  next(new AppError(`Cannot find ${req.originalUrl} in this server`, 404));
 });
 
 // Global Error Handling
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
