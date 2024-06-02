af159d89f561e780ce4d9e51b1f52e1c0c1f2930
Sanjil Shakya <sanjil@noveltytechnology.com>
2024-06-02 05:56:44 -0400

    Global Error Handling Middleware Implemented

--- app.js
+++ app.js
@@ -7,10 +7,28 @@ app.use(express.json());
 
 app.use("/api/v1/tours", tourRouter);
 
+//Handling Unhandled Routes
 app.all("*", (req, res, next) => {
-  res.status(400).json({
-    status: "fail",
-    message: `Cannot find ${req.originalUrl} in this server`,
+  //   res.status(404).json({
+  //     status: "fail",
+  //     message: `Cannot find ${req.originalUrl} in this server`,
+  //   });
+
+  const error = new Error(`Cannot find ${req.originalUrl} in this server`);
+  error.status = "fail";
+  error.statusCode = 404;
+
+  next(error);
+});
+
+// Global Error Handling
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
